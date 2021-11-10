735cf17ae47ed247c3b65cf13c0decc92caa31dd
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-10 17:40:04 +0600

    spinner added

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,11 @@
+import { CircularProgress } from '@mui/material';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import useAuth from '../Hooks/useAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
     const { user, isLoading } = useAuth();
-    if (isLoading) { return 'Laoding' }
+    if (isLoading) { return <div className="spinner" style={{ textAlign: 'center' }}><CircularProgress /></div> }
     return (
         <Route
             {...rest}
