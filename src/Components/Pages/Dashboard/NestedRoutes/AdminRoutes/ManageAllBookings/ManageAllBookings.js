103545539ad7078dc4b8530a7f57715acbfc80b7
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-11 17:04:09 +0600

    spinner added in manage all bookings

--- src/Components/Pages/Dashboard/NestedRoutes/AdminRoutes/ManageAllBookings/ManageAllBookings.js
+++ src/Components/Pages/Dashboard/NestedRoutes/AdminRoutes/ManageAllBookings/ManageAllBookings.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import ApartmentBox from '../../RouteShared/ApartmentBox';
 
 const ManageAllBookings = () => {
+    const [allBookings, setallBookings] = useState([]);
+    const [isloading, setisloading] = useState(true);
+    useEffect(() => {
+        fetch('http://localhost:5000/bookedapartments')
+            .then(res => res.json())
+            .then(data => {
+                setallBookings(data);
+                setisloading(false);
+            })
+    }, [])
     return (
         <div>
-            <h2>Mange all bookings</h2>
+            <h2>We have total {allBookings.length} Bookings</h2>
+            {
+                isloading ?
+                    <div style={{ textAlign: 'center' }}>
+                        <CircularProgress />
+                    </div>
+                    :
+                    <>
+                        {
+                            allBookings.map(aprt => <ApartmentBox key={aprt._id} apartmentData={aprt} />)
+                        }
+                    </>
+            }
         </div>
     );
 };
