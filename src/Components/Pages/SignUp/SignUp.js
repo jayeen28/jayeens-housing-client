b4cdfdea8ff09f2149b1f1eb9de59c1359bd61e5
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-13 13:36:27 +0600

    sign up page done

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -1,10 +1,11 @@
-import { Button, Container, TextField, Typography } from '@mui/material';
+import { Container, TextField } from '@mui/material';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
+import './SignUp.css';
 
 const SignUp = () => {
     const { userSignup } = useAuth()
@@ -19,16 +20,19 @@ const SignUp = () => {
         <div className="signin-page">
             <Header />
             <Container>
-                This is sign up page
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <h2 className="jsectionhead">sign up</h2>
+                <form onSubmit={handleSubmit(onSubmit)} className="signup-form">
                     <TextField id="outlined-basic" label="Your name" {...register('userName')} variant="outlined" />
                     <TextField id="outlined-basic" label="Your email" {...register('userEmail')} variant="outlined" />
                     <TextField id="outlined-basic" label="Your password" {...register('userPass')} variant="outlined" />
-                    <Button type="submit" variant="contained">Contained</Button>
+                    <button type="submit" className="jbutton">Contained</button>
                 </form>
-                <Typography>
-                    Already signed up? please <Link to='/signin'>Sign In</Link>
-                </Typography>
+                <p style={{ textAlign: 'center' }}>
+                    Already signed up? please <Link to='/signin' style={{
+                        color: '#1D6B6F', fontWeight: 'bold',
+                        textDecoration: 'underline'
+                    }}>Sign in</Link>
+                </p>
             </Container>
             <Footer />
         </div>
