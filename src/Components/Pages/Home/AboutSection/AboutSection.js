3b6c3e2dd57c36beb53a66bf579e06b0435eeb2d
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-13 22:59:45 +0600

    spelling correction

--- src/Components/Pages/Home/AboutSection/AboutSection.js
+++ src/Components/Pages/Home/AboutSection/AboutSection.js
@@ -17,7 +17,7 @@ const AboutSection = () => {
                     <Grid item xs={12} sm={12} md={6}>
                         <div className="abt-text">
                             <p style={{ color: '#1D6B6F', margin: '5px 0px' }}>ABOUT US</p>
-                            <h2>We will make your dream house come true.</h2>
+                            <h2>We will make your dream house comes true.</h2>
                             <p>We are one of the best service provider and this will help you make a good property in the easiest way and this is one of the best and a proper way to buy of rent your suitable flat.</p>
                             <Link to="/apartments" className="jbutton">Book now !</Link>
                         </div>
