682899899bdb902653f5b20e2f437c88f7ebf79d
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-27 15:13:18 +0600

    another button added at manage all orders

--- src/Components/Pages/Dashboard/NestedRoutes/AdminRoutes/ManageAllBookings/ManageAllBookings.js
+++ src/Components/Pages/Dashboard/NestedRoutes/AdminRoutes/ManageAllBookings/ManageAllBookings.js
@@ -6,10 +6,10 @@ const ManageAllBookings = () => {
     const [allBookings, setallBookings] = useState([]);
     const [isloading, setisloading] = useState(false);
     const [render, setrender] = useState(false);
-    const [displayBookingStat, setdisplayBookingStat] = useState('paid');
+    const [displayBookingStat, setdisplayBookingStat] = useState('Confirmed');
     useEffect(() => {
         setisloading(true)
-        fetch(`https://afternoon-earth-46164.herokuapp.com/bookedapartments/${displayBookingStat}`)
+        fetch(`http://localhost:5000/bookedapartments/${displayBookingStat}`)
             .then(res => res.json())
             .then(data => {
                 setallBookings(data);
@@ -21,8 +21,9 @@ const ManageAllBookings = () => {
             <h2 className="jsectionhead">Manage all bookings<small style={{ display: 'block', fontSize: '14px', color: '#1D6B6F' }}>Total {allBookings.length} {displayBookingStat} bookings</small></h2>
 
             <div className="managebookings-btn" style={{ textAlign: 'center' }}>
-                <button className="jbutton" style={{ margin: '0px 5px' }} onClick={() => setdisplayBookingStat('paid')}>Paid</button>
-                <button className="jbutton" style={{ margin: '0px 5px' }} onClick={() => setdisplayBookingStat('unpaid')}>Unpaid</button>
+                <button className="jbutton" style={{ margin: '0px 5px' }} onClick={() => setdisplayBookingStat('Confirmed')}>Confirmed</button>
+                <button className="jbutton" style={{ margin: '0px 5px' }} onClick={() => setdisplayBookingStat('Processing')}>Paid</button>
+                <button className="jbutton" style={{ margin: '0px 5px' }} onClick={() => setdisplayBookingStat('Unpaid')}>Unpaid</button>
             </div>
 
             {
